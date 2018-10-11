f79fda06b18cf1108f780bb5dd7f96758d2f2d3d
Rahul Zore <zore.r@husky.neu.edu>
2018-10-10 22:39:19 -0400

    Client setup using react and display books from server on client

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect('mongodb://admin123:admin123@ds227853.mlab.com:27853/gql-zore', { useNewUrlParser: true });
 
